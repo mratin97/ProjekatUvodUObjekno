d18a43713d20cd889c9e0b8dd8aa9560050686cb
mratin97 <48236938+mratin97@users.noreply.github.com>
2019-05-31 22:32:03 +0200

    Add files via upload

--- Projekat/src/domZdravlja/DomZdravlja.java
+++ Projekat/src/domZdravlja/DomZdravlja.java
@@ -14,7 +14,7 @@ import paket1.MedSestra;
 import paket1.Pacijent;
 import paket1.Zaposleni;
 import pregled.Pregled;
-
+import java.util.*;
 public class DomZdravlja {
 
 	private ArrayList<MedSestra> sestre;
@@ -87,5 +87,152 @@ public class DomZdravlja {
 			e.printStackTrace();
 		}
 	}
-	
+	 public void DodajSestru() throws IOException {
+ 		
+ 		BufferedWriter bw = new BufferedWriter( new FileWriter("src/fajlovi/medicinskasestra.txt",true) );
+ 		Scanner strInput = new Scanner(System.in);
+ 		
+ 		String ime, prezime, jmbg, adresa, brojTelefona, korisnickoIme, lozinka, pol, plata;
+ 		
+ 		System.out.print("Enter the Employee ime: ");
+ 		ime= strInput.nextLine();
+ 		System.out.print("Enter the Employee prezime: ");
+ 		prezime = strInput.nextLine();
+ 		System.out.print("Enter the Employee jmbg: ");
+ 		jmbg = strInput.nextLine();
+ 		System.out.print("Enter the Employee adresa: ");
+ 		adresa = strInput.nextLine();    
+ 		System.out.print("Enter the Employee brojTelefona: ");
+ 		brojTelefona = strInput.nextLine();   
+ 		System.out.print("Enter the Employee korisnickoIme : ");
+ 		korisnickoIme = strInput.nextLine();   
+ 		System.out.print("Enter the Employee lozinka:");
+ 		lozinka = strInput.nextLine(); 
+ 		System.out.print("Enter the Employee pol:");
+ 		pol = strInput.nextLine(); 
+ 		System.out.print("Enter the Employee plata:");
+ 		plata = strInput.nextLine(); 
+ 		   		
+ 		bw.write(ime+"|"+prezime+"|"+jmbg+"|"+adresa+"|"+brojTelefona+"|"+korisnickoIme+"|"+lozinka+"|"+pol+"|"+plata);
+ 		bw.flush();
+ 		bw.newLine();
+ 		bw.close();		
+ 	
+ }
+	 public void DeleteRecordByID() throws IOException {
+ 		Scanner strInput =  new Scanner(System.in);
+ 		String korisnickoIme, record;
+ 		
+ 		
+ 		File tempDB = new File("src/fajlovi/medicinskasestraTemp.txt");
+ 		File db = new File("src/fajlovi/medicinskasestra.txt");
+ 		
+ 		
+ 		BufferedReader br = new BufferedReader( new FileReader( db ) );
+ 		BufferedWriter bw = new BufferedWriter( new FileWriter( tempDB ) );
+ 		
+ 		
+ 		System.out.println("\t\t Delete Employee Record\n");
+ 		
+ 		System.out.println("Enter the Employee ID: ");
+ 		korisnickoIme =  strInput.nextLine();
+ 		
+ 		
+ 		while( ( record = br.readLine() ) != null ) {
+ 			
+ 			
+ 			if( record.contains(korisnickoIme) ) 
+ 				continue;
+
+ 			bw.write(record);
+ 			bw.flush();
+ 			bw.newLine();
+
+ 		}
+ 		
+ 		br.close();
+ 		bw.close();
+ 		
+ 		db.delete();
+ 		
+ 		tempDB.renameTo(db);
+
+ }
+	 
+	 public void izmeniSestru() throws IOException {
+ 		String newIme, newPrezime, newJmbg, newAdresa, newBrojTelefona, newKorisnickoIme, newLozinka, newPol, newPlata, record, korisnickoIme,record2;
+ 		
+ 		File tempDB = new File("src/fajlovi/medicinskasestraTemp.txt");
+ 		File db = new File("src/fajlovi/medicinskasestra.txt");
+ 		
+ 		BufferedReader br = new BufferedReader( new FileReader(db) );
+ 		BufferedWriter bw = new BufferedWriter( new FileWriter(tempDB) );
+ 		    		
+ 		Scanner strInput = new Scanner(System.in);
+ 		
+ 		System.out.println("\t\t Update Employee Record\n\n");   
+		/****/		
+			System.out.println("Enter the Employee ID: ");
+			korisnickoIme = strInput.nextLine();	    		
+	    		System.out.println(" ------------------------------------------------------------------------------------------------------- ");
+	    		System.out.println("|	ime		newPrezime 	newJmbg		Address		 newBrojTelefona"
+	    				+ "     newKorisnickoIme 	newLozinka		newPol 		newPlata|");
+	    		System.out.println(" ------------------------------------------------------------------------------------------------------- ");
+	    		
+	    		while( ( record = br.readLine() ) != null ) {
+	    			
+	    			StringTokenizer st = new StringTokenizer(record,"|");
+	    			if( record.contains(korisnickoIme) ) {
+	    				System.out.println("|	"+st.nextToken()+"		"+st.nextToken()+"     	"+st.nextToken()+"		"+st.nextToken()+"  	  "+st.nextToken()+"     "+st.nextToken()+" 	"
+	    						+ " 	     	"+st.nextToken()+" 		"+st.nextToken()+"        " +st.nextToken()+"	 |");
+	    			}
+	    			
+	    		}	    		
+	    		System.out.println("|	                                            	          |");
+	    		System.out.println(" ------------------------------------------------------------- ");
+	    		
+	    	br.close();
+		/****/    	   
+	    	System.out.print("Enter the Employee ime: ");
+	    	newIme= strInput.nextLine();
+	 		System.out.print("Enter the Employee prezime: ");
+	 		newPrezime = strInput.nextLine();
+	 		System.out.print("Enter the Employee jmbg: ");
+	 		newJmbg = strInput.nextLine();
+	 		System.out.print("Enter the Employee adresa: ");
+	 		newAdresa = strInput.nextLine();    
+	 		System.out.print("Enter the Employee brojTelefona: ");
+	 		newBrojTelefona = strInput.nextLine();   
+	 		System.out.print("Enter the Employee korisnickoIme : ");
+	 		newKorisnickoIme = strInput.nextLine();   
+	 		System.out.print("Enter the Employee lozinka:");
+	 		newLozinka = strInput.nextLine(); 
+	 		System.out.print("Enter the Employee pol:");
+	 		newPol = strInput.nextLine(); 
+	 		System.out.print("Enter the Employee plata:");
+	 		newPlata = strInput.nextLine(); 
+ 		
+ 		BufferedReader br2 = new BufferedReader( new FileReader(db) );
+ 			
+ 		while( (record2 = br2.readLine() ) != null ) {    			
+ 			if(record2.contains(korisnickoIme)) {
+ 				bw.write(newIme+"|"+newPrezime+"|"+newJmbg+"|"+newAdresa+"|"+newBrojTelefona+"|"+newKorisnickoIme+"|"+newLozinka+"|"+newPol+"|"+newPlata);
+ 			} else {
+ 			
+ 				bw.write(record2);	
+ 			}    			
+ 			bw.flush();
+ 			bw.newLine();
+ 		}
+ 		
+ 		bw.close();
+ 		br2.close();    		
+ 		db.delete();    		
+ 		boolean success = tempDB.renameTo(db);    		
+		System.out.println(success);    		
+ 		    		
+ 		
+ }
+	 
+	 
 }
--- Projekat/src/main/Main.java
+++ Projekat/src/main/Main.java
@@ -1,22 +1,36 @@
 package main;
+import java.io.IOException;
 
-import java.util.ArrayList;
 import domZdravlja.DomZdravlja;
 import paket1.MedSestra;
 
-
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		DomZdravlja dom=new DomZdravlja();
 		dom.ucitajZaposlene("medicinskasestra.txt");
-		MedSestra testsestra = new MedSestra("Dusan", "Mratinkovic", "4144564456", "Adtresa", "06325458", "admin", "admin", "M",123);
-		dom.dodajProdavca(testsestra);
+		//MedSestra testsestra = new MedSestra("Dusan", "Mratinkovic", "4144564456", "Adtresa", "06325458", "admin", "admin", "M",123);
+		//dom.dodajProdavca(testsestra);
 		dom.snimiZaposlene("medicinskasestra.txt");
 		ispisiSvePodatke(dom);
+		try 
+        {
+            dom.izmeniSestru();
+        }
+        catch (IOException ioe)  {
+        }	
+		try 
+        {
+            dom.DodajSestru();
+        }
+        catch (IOException ioe)  {
+        	}
 		
 		
-	}
+        }
+	
+        
+	
 
 	public static void ispisiSvePodatke(DomZdravlja dom) {
 
